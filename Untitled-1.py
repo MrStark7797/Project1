0c865f4493b26d08844dbec6fb91d27f6cc59c51
MrStark7797 <starkiewill@gmail.com>
2024-10-01 16:34:11 +0100

    Name finder using email with website

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,22 +1,14 @@
 from urllib.request import urlopen
-
 def findNameFromEmail(email):
-    x = email.find("@")
-    y = email[:x]
-    weblink = "https://www.ecs.soton.ac.uk/people/" + y
-    website = urlopen(weblink)
-    found = False
+    y = email[:email.find("@")]
+    website, found =urlopen("https://www.ecs.soton.ac.uk/people/" + y), bool(False)
     while found == False:
-        z = website.readline()
-        z = str(z)
-        check =z.find("heading-m inline-block text-prussianDark")
-        if check != -1:
-            found = True
-            startname = z.find(">") + 1
-            endname = z.find("/") - 1
+        z = str(website.readline())
+        if z.find("heading-m inline-block text-prussianDark") != -1:
+            found, startname, endname = True, z.find(">") + 1, z.find("/") - 1
             name = z[startname:endname]
             print(f"{y}@soton.ac.uk is {name}")
-
 emailFile = open("emails.txt", "r")
 for x in emailFile:
     findNameFromEmail(x)
+emailFile.close()
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-x = "Hello I am dave"
-print(x[(x.find("m")) + 1:])
+from urllib.request import urlopen
+def findNameFromEmail(email):
+    y = email[:email.find("@")]
+    website, found =urlopen("https://www.ecs.soton.ac.uk/people/" + y), bool(False)
+    while found == False:
+        z = str(website.readline())
+        if z.find("heading-m inline-block text-prussianDark") != -1:
+            found, startname, endname = True, z.find(">") + 1, z.find("/") - 1
+            name = z[startname:endname]
+            print(f"{y}@soton.ac.uk is {name}")
+emailFile = open("emails.txt", "r")
+while True:
+    print("Waiting For email\n")
+    email = input()
+    if email == "":
+        break
+    findNameFromEmail(email)
